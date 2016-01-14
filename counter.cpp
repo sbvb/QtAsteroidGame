a9ecd31f6bf200261992ea0c246a33e4f9833e9e
Lucas Barcellos <lucas.barcellos.oliveira@gmail.com>
2016-01-14 20:53:26 -0200

    Create counter.cpp

---
+++ counter.cpp
@@ -0,0 +1,35 @@
+#include "counter.h"
+#include <QFont>
+#include <QMessageBox>
+
+#define windowwidth 500
+
+extern bool isplaying;
+
+Counter::Counter(QGraphicsItem *parent, int timelimit)
+{
+    m_time=timelimit;
+    setPlainText(QString("Time: ")+QString::number(m_time));
+    setDefaultTextColor(Qt::red);
+    setFont(QFont("magneto",16));
+}
+
+void Counter::decrease()
+{
+    if(m_time>0){
+        m_time--;
+        setPlainText(QString("Time: ")+QString::number(m_time));
+    }
+    else{
+        isplaying=false;
+        setPlainText(QString("Time's up!"));
+        setFont(QFont("magneto",32));
+        setPos(windowwidth/2,y());
+        static int endmessage=QMessageBox::critical(0,"Time's up!","Your time limit has been reached!");
+    }
+}
+
+int Counter::getCounter()
+{
+    return m_time;
+}
